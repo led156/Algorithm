bfef4ddde2582b1e943a1e99cc0f490542557504
eunbi lee <led156@naver.com>
2022-09-26 20:42:43 +0900

    집합

--- baekjoon/n11723.cpp
+++ baekjoon/n11723.cpp
@@ -6,41 +6,41 @@ using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int M, ans;
+    int M, ans = 0;
     cin >> M;
 
     string operators;
     int operands;
-    for (int i = 0; i < M; ++i) {
+    while (M--) {
+        operators.clear();
         cin >> operators;
 
         if (operators == "add") {
             cin >> operands;
-            ans = ans | (1<<operands);
+            ans |= (1<<operands);
         }
         else if (operators == "remove") {
             cin >> operands;
-            ans = ans & ~(1<<operands);
+            ans &= ~(1<<operands);
         }
         else if (operators == "check") {
             cin >> operands;
             if (ans & (1 << operands)) 
-                cout << 1 << "\n";
+                cout << 1 << '\n';
             else 
-                cout << 0 << "\n";
+                cout << 0 << '\n';
         }
         else if (operators == "toggle") {
             cin >> operands;
-            ans = ans ^ (1<<operands);
+            ans ^= (1<<operands);
         }
         else if (operators == "all") {
             ans = (1 << 21) - 1;
         }
-        else {
+        else if (operators == "empty") {
             ans = 0;
         }
     }
-
 
     return 0;
 }
