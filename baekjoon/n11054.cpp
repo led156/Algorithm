4bcffd96b48a3561f10beb84c07dbe649b7f9ce0
eunbi lee <led156@naver.com>
2022-11-10 20:33:20 +0900

    가장 긴 바이토닉 부분 수열

--- baekjoon/n11054.cpp
+++ baekjoon/n11054.cpp
@@ -12,28 +12,29 @@ int main() {
         scanf("%d",&A[i]);
     }
 
+    /* 1. 왼쪽; 자신보다 더 작은 원소 -> 증가하는 부분수열 체크 */
     for (int i = 1; i <= N; i++) {
+        memo[i][0] = 1;
         for (int j = 0; j < i; j++) {
             if (A[i]>A[j]) {
                 memo[i][0] = max(memo[i][0], memo[j][0]+1);
             }
         }
-        for (int j = N+1; j > i; j--) {
-            if (A[i]>A[j]) {
-                memo[i][1] = max(memo[i][1], memo[j][1]+1);
-            }
-        }
     }
 
-    for (int i = N; i > 0; i--) {
+    /* 2. 오른쪽; 자신보다 더 작은 원소 -> 감소하는 부분수열 체크 */
+    for (int i = N; i >= 1; i--) {
+        memo[i][1] = 1;
         for (int j = N; j > i; j--) {
             if (A[i]>A[j]) {
                 memo[i][1] = max(memo[i][1], memo[j][1]+1);
             }
         }
-        //printf("%d : %d + %d\n", i, memo[i][0], memo[i][1]);
+
+        /* 최대값(정답) 계산 */
         ans = max(ans, memo[i][0]+memo[i][1]);
     }
+
 
     cout << ans-1;
     return 0;
