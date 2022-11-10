b1c7a6e1a85616014d9ff475c9434f4f2ef1dc9b
eunbi lee <led156@naver.com>
2022-11-10 22:45:09 +0900

    이진 검색 트리

--- baekjoon/n5639.cpp
+++ baekjoon/n5639.cpp
@@ -16,7 +16,7 @@ void find_(int start, int end) {
     int root_num = pre_order[start];
     int idx = start;
 
-    while (idx < end && root_num > pre_order[++idx]);
+    while (idx < end && root_num > pre_order[++idx]); // 자신을 기준으로 왼쪽 노드들은 자신보다 노드 값이 작고, 오른쪽 노드들은 자신보다 노드 값이 크다. 따라서 자신보다 큰 값이 처음 나오는 곳을 찾으면 오른쪽 자식을 구할 수 있다.
 
     find_(start+1, idx);
     find_(idx, end);
