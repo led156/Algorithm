b52d4143e41c55d9a24118caa2bef863abbf78d7
eunbi lee <led156@naver.com>
2023-07-25 03:05:59 +0900

    [백준] 스도쿠

--- baekjoon/n2580.cpp
+++ baekjoon/n2580.cpp
@@ -2,73 +2,65 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+#define MAX_LEN 9
 
-int board[9][9];
-vector<pair<int, int> > empty_space;
+int board[MAX_LEN][MAX_LEN];
+vector<pair<int, int> > empty_list;
 
-void print_sudoku() {
-    for (int i = 0; i < 9; ++i) {
-        for (int j = 0; j < 9; ++j) {
-            cout << board[i][j] << " ";
-        }
-        cout << "\n";
-    }
-}
-
-bool ispromising(int x, int y, int num) {
-    for (int i = 0; i < 9; i++) {
-        if (board[x][i] == num)
-            return false;
+bool promising(int num, int x, int y) {
+    for (int i = 0; i < MAX_LEN; ++i) {
+        if (board[i][y] == num) return false;
     }
 
-    for (int i = 0; i < 9; i++) {
-        if (board[i][y] == num)
-            return false;
+    for (int i = 0; i < MAX_LEN; ++i) {
+        if (board[x][i] == num) return false;
     }
 
-    int ni = (x/3)*3;
-    int nj = (y/3)*3;
-
-    for (int i = 0; i < 3; i++) {
+    int gridx = (x/3)*3, gridy = (y/3)*3;
+    for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            if (board[i+ni][j+nj] == num)
-                return false;
+            if (board[gridx+i][gridy+j] == num) return false;
         }
     }
-
     return true;
 }
 
-void dfs(int idx) {
-    if (idx == empty_space.size()) {
-        print_sudoku();
+void print_board() {
+    for (int i = 0; i < MAX_LEN; ++i) {
+        for (int j = 0; j < MAX_LEN; ++j) {
+            cout << board[i][j] << " ";
+        }
+        cout << '\n';
+    }
+}
+
+void bt(int idx) {
+    if (idx >= empty_list.size()) {
+        print_board();
         exit(0);
     }
 
-    int x = empty_space[idx].first;
-    int y = empty_space[idx].second;
-    for (int i = 1; i <= 9; i++) {
-        if (ispromising(x, y, i)) {
-            board[x][y] = i;
-            dfs(idx+1);
+    int x = empty_list[idx].first, y = empty_list[idx].second;
+
+    for (int num = 1; num <= MAX_LEN; ++num) {
+        if (promising(num, x, y)) {
+            board[x][y] = num;
+            bt(idx+1);
             board[x][y] = 0;
         }
     }
 }
 
+
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
+    for (int i = 0; i < MAX_LEN; ++i) {
+        for (int j = 0; j < MAX_LEN; ++j) {
             cin >> board[i][j];
-            if (board[i][j] == 0) {
-                empty_space.push_back(make_pair(i, j));
-            }
+            if (board[i][j] == 0) empty_list.push_back({i, j});
         }
     }
 
-    dfs(0);
+    bt(0);
 
     return 0;
 }
