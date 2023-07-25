902f7661a2dc0a4ab2eabfd318c3c7a603f72fe7
eunbi lee <led156@naver.com>
2023-07-25 11:01:21 +0900

    [백준] 수 찾기

--- baekjoon/n1920.cpp
+++ baekjoon/n1920.cpp
@@ -3,27 +3,28 @@
 #include <algorithm>
 using namespace std;
 
-int N, M;
+int N, M, k;
+vector<int> A;
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
     cin >> N;
-    vector<int> A(N, 0);
+
     for (int i = 0; i < N; ++i) {
-        cin >> A[i];
+        cin >> k;
+        A.push_back(k);
     }
+
     sort(A.begin(), A.end());
 
     cin >> M;
-    int num;
-    for (int i = 0; i < M; ++i) {
-        cin >> num;
-        int cnt = upper_bound(A.begin(), A.end(), num) - lower_bound(A.begin(), A.end(), num);
 
-        if (cnt > 0) cout << 1 << '\n';
-        else cout << 0 << '\n';
+    for (int i = 0; i < M; ++i) {
+        cin >> k;
+        if (upper_bound(A.begin(), A.end(), k) - lower_bound(A.begin(), A.end(), k)) cout << "1\n";
+        else cout << "0\n";
     }
+
 
     return 0;
 }
