d893e3d0ba34a27cf118aba5e5c5aef1e3835046
eunbi lee <led156@naver.com>
2023-07-31 14:29:25 +0900

    [백준] 네트워크 연결

--- baekjoon/n1922.cpp
+++ baekjoon/n1922.cpp
@@ -2,41 +2,79 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+#define MAX_N 1001
 
-int N, M;
-vector<pair<int, pair<int, int> > > ver;
-int parent[1001];
+struct edge
+{
+    /*
+        cost : 비용
+        A : 노드1
+        B : 노드2
+    */
+    int cost;
+    int A;
+    int B;
+};
 
-int find_parent(int node) {
-    if (parent[node] != node) return find_parent(parent[node]);
-    return node;
+bool comp(edge& e1, edge& e2) {
+    if (e1.cost == e2.cost){
+        if (e1.A == e2.A) return e1.B < e2.B;
+        else return e1.A < e2.A;
+    }
+    else return e1.cost < e2.cost;
+}
+
+int N, M, a, b, c, cnt = 0, ans = 0;
+int parent[MAX_N];
+vector<edge> edges;
+
+void init_parent() {
+    for (int i = 1; i <= N; ++i) {
+        parent[i] = i;
+    }
+}
+
+int find_parent(int a) {
+    if (a == parent[a]) return a;
+    return parent[a] = find_parent(parent[a]);
+}
+
+void union_parent(int a, int b) {
+    int ap = parent[a];
+    int bp = parent[b];
+
+    if (ap!=bp) {
+        if (ap >= bp) {
+            parent[bp] = ap;
+        }
+        else {
+            parent[ap] = bp;
+        }
+    }
 }
 
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> N >> M;
-    int a, b, c;
-    for (int i = 0; i < M; i++) {
+    init_parent();
+
+    for (int i = 0; i < M; ++i) {
         cin >> a >> b >> c;
-        ver.push_back(make_pair(c, make_pair(a, b)));
-    }
-    sort(ver.begin(), ver.end());
-    for (int i = 1; i < N; i++) {
-        parent[i] = i;
+        edges.push_back({c, a, b});
     }
 
-    int node1, node2, cost, n1_parent, n2_parent, ans = 0;
-    for (int i = 0; i < ver.size(); i++) {
-        node1 = ver[i].second.first;
-        node2 = ver[i].second.second;
-        cost = ver[i].first;
-        n1_parent = find_parent(node1);
-        n2_parent = find_parent(node2);
-        if (n1_parent != n2_parent) {
-            ans += cost;
-            parent[n2_parent] = n1_parent;
-        }
+    sort(edges.begin(), edges.end(), comp);
+
+    for (int i = 0; i < edges.size() && cnt < N-1; ++i) {
+        edge e = edges[i];
+        if (find_parent(e.A) == find_parent(e.B)) continue;
+        
+        ans += e.cost;
+        ++cnt;
+        union_parent(e.A, e.B);
     }
 
     cout << ans;
+
     return 0;
 }
