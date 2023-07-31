8346a7ff3dae93c70afcf24eb92f6e64c042447a
eunbi lee <led156@naver.com>
2023-07-31 23:33:14 +0900

    [백준] 키 순서

--- baekjoon/n2458.cpp
+++ baekjoon/n2458.cpp
@@ -1,48 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cstring>
+#include <queue>
 using namespace std;
+#define MAX_N 501
 
-int N, M;
-vector<int> smaller_than[501], bigger_than[501];
-bool visited[501];
-int cnt = 0, ans = 0;
 
-void dfs(vector<int> arr[], int cur) {
-    visited[cur] = true;
+int N, M, a, b, ans = 0;
+vector<int> graph[2][MAX_N];
+int visited[MAX_N];
 
-    for (int i = 0; i < arr[cur].size(); ++i) {
-        int nxt = arr[cur][i];
+int bfs(int mode, int node) {
+    queue<int> que;
+    que.push(node);
+    visited[node] = node;
+    int cnt = 0;
 
-        if (visited[nxt]) continue;
+    while (!que.empty()) {
+        int cur_node = que.front();
+        que.pop();
 
-        ++cnt;
-        dfs(arr, nxt);
+        for (int i = 0; i < graph[mode][cur_node].size(); ++i) {
+            int nxt_node = graph[mode][cur_node][i];
+            if (visited[nxt_node]!=node) {
+                visited[nxt_node] = node;
+                que.push(nxt_node);
+                ++cnt;
+            }
+        }
     }
+
+    return cnt;
 }
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> N >> M;
-    
-    int a, b;
     for (int i = 0; i < M; ++i) {
-        cin >> a >> b; // a -> b
-        smaller_than[b].push_back(a);
-        bigger_than[a].push_back(b);
+        cin >> a >> b; // a < b
+        graph[0][a].push_back(b);
+        graph[1][b].push_back(a);
     }
 
-    for (int i = 1; i <= N; ++i) {
-        cnt = 0;
-
-        memset(visited, false, sizeof(visited));
-        dfs(smaller_than, i);
-
-        memset(visited, false, sizeof(visited));
-        dfs(bigger_than, i);
-
-        if (cnt == N-1) ++ans;
+    for (int node = 1; node <= N; ++node) {
+        if (N-1 == (bfs(0, node) + bfs(1, node))) ++ans;
     }
 
     cout << ans;
