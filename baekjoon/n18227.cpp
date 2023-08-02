25f2284504fda583b6f287739723eb2a272b6368
eunbi lee <led156@naver.com>
2023-08-02 10:45:43 +0900

    [백준] 성대나라의 물탱크

--- baekjoon/n18227.cpp
+++ baekjoon/n18227.cpp
@@ -1,72 +1,65 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
 using namespace std;
 #define MAX_N 200001
-#define MAX_H 21
+typedef long long ll;
 
-int N, C, x, y, Q, q1, q2;
+int N, C, x, y, Q, q1, q2, k;
 vector<vector<int>> edges(MAX_N);
-bool visited[MAX_N];
-int parent[MAX_H][MAX_N];
-int depth[MAX_N], water[MAX_N];
+int node[MAX_N], node_range[MAX_N][2];
+int depth[MAX_N];
+ll water[4*MAX_N];
 
-void init_tree() {
-    queue<int> que;
-    que.push(C);
-    visited[C] = true;
-    depth[C] = 1;
 
-    while(!que.empty()) {
-        int cur_node = que.front();
-        que.pop();
+void dfs(int cur) {
+	node[cur] = node_range[cur][0] = ++k;
 
-        for (int i = 0; i < edges[cur_node].size(); ++i) {
-            int nxt_node = edges[cur_node][i];
-            if (visited[nxt_node]) continue;
-            visited[nxt_node] = true;
-            que.push(nxt_node);
-            depth[nxt_node] = depth[cur_node] + 1;
-            parent[0][nxt_node] = cur_node;
-            for (int k = 1; k < MAX_H; ++k) {
-                parent[k][nxt_node] = parent[k-1][parent[k-1][nxt_node]];
-            }
-        }
-    }
+	for (int nxt : edges[cur]) {
+		if (depth[nxt]>0) continue;
+		depth[nxt] = depth[cur] + 1;
+		dfs(nxt);
+	}
+
+	node_range[cur][1] = k;
 }
 
-void fill_water(int cur_node) {
-    // cout <<"->" <<cur_node<<'\n';
-    
-    while (true) {
-        water[cur_node] += depth[cur_node];
-        if (cur_node == C) break;
-        cur_node = parent[0][cur_node];
-    }
+ll update(int cur_node, int left, int right, int target_node) {
+	if (target_node < left || right < target_node) return water[cur_node];
+	if (left == right) return ++water[cur_node];
+	return water[cur_node] = update(cur_node * 2, left, (left + right) / 2, target_node) + update(cur_node * 2 + 1, (left + right) / 2+1, right, target_node);
 }
+
+ll search(int cur_node, int left, int right, int target_left, int target_right) {
+	if (target_right < left || right < target_left) return 0;
+	if (target_left <= left && right <= target_right) return water[cur_node];
+	return search(cur_node * 2, left, (left + right) / 2, target_left, target_right) + search(cur_node * 2 + 1, (left + right) / 2+1, right, target_left, target_right);
+}
+
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> N >> C;
-    for (int i = 0; i < N-1; ++i) {
-        cin >> x >> y;
-        edges[x].push_back(y);
-        edges[y].push_back(x);
-    }
-    init_tree();
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> C;
+	for (int i = 0; i < N - 1; ++i) {
+		cin >> x >> y;
+		edges[x].push_back(y);
+		edges[y].push_back(x);
+	}
+	
+	depth[C] = 1;
+	dfs(C);
 
-    cin >> Q;
-    for (int i = 0; i < Q; ++i) {
-        cin >> q1 >> q2;
-        if (q1 == 1) { // 물 채우기
-            fill_water(q2);
-        }
-        else { // 출력
-            cout << water[q2] << '\n';
-        }
-    }
+	cin >> Q;
+	for (int i = 0; i < Q; ++i) {
+		cin >> q1 >> q2;
+		if (q1 == 1) { // 물 채우기
+			update(1, 1, N, node[q2]);
+		}
+		else { // 출력
+			cout << search(1, 1, N, node_range[q2][0], node_range[q2][1])*depth[q2] << '\n';
+		}
+	}
 
 
-    return 0;
+	return 0;
 }
