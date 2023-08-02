ee96e4647f051a8f61190df2393e7c6b00cc62e6
eunbi lee <led156@naver.com>
2023-08-02 12:57:11 +0900

    [백준] 플로이드

--- baekjoon/n11404.cpp
+++ baekjoon/n11404.cpp
@@ -1,52 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
 using namespace std;
+#define MAX_n 101
 
-int n, m;
-int dist[101][101];
+int n, m, a, b, c;
+int dist[MAX_n][MAX_n];
 
-void floyd(int middle) {
-    for (int i = 1; i <= n; i++) {
-        if (i != middle) {
-            for (int j = 1; j <= n; j++) {
-                if (dist[i][middle] == -1 || dist[middle][j] == -1) continue;
-                if (dist[i][j] == -1) {
-                    dist[i][j] = dist[i][middle] + dist[middle][j];
-                }
-                else {
-                    dist[i][j] =  min(dist[i][j], dist[i][middle] + dist[middle][j]);
-                }
-            }
-        }
-    }
-}
+
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL);
-    cin >> n >> m;
-    int cost, a, b;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (i!=j) dist[i][j] = -1;
-        }
-    }
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+	memset(dist, -1, sizeof(dist));
+	for (int i = 0; i < m; ++i) {
+		cin >> a >> b >> c;
+		if (dist[a][b] == -1) dist[a][b] = c;
+		else dist[a][b] = min(dist[a][b], c);
+	}
 
-    for (int i = 1; i <= m; i++) {
-        cin >> a >> b >> cost;
-        if (dist[a][b] != -1) dist[a][b] = min(dist[a][b], cost);
-        else dist[a][b] = cost;
-    }
+	for (int k = 1; k <= n; ++k) {
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= n; ++j) {
+				if (i == j || i == k || j == k || dist[i][k] == -1 || dist[k][j] == -1) continue;
 
-    for (int i = 1; i <= n; i++) {
-        floyd(i);
-    }
+				if (dist[i][j] != -1) dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+				else dist[i][j] = dist[i][k] + dist[k][j];
+				
+			}
+		}
+	}
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (dist[i][j] == -1) cout << 0 << " ";
-            else cout << dist[i][j] << " ";
-        }
-        cout << "\n";
-    }
 
-    return 0;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			if (dist[i][j] == -1) cout << "0 ";
+			else cout << dist[i][j] << " ";
+		}
+		cout << '\n';
+	}
+
+	return 0;
 }
